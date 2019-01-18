2b90cd92487625bdd509d3181d7c2306ffee144d
Aishwarya <aishwaryarnair5@gmail.com>
2019-01-18 11:31:11 +0530

    File committed

--- application/controllers/Search_student.php
+++ application/controllers/Search_student.php
@@ -7,18 +7,51 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Search_student extends CI_Controller
 {
-
     public function index()
     {
 
         $from_age = $this->input->get('from_age');
         $to_age = $this->input->get('to_age');
         $data = $this->mongo_db->where_between('age', $from_age, $to_age)->get('student');
+        $this->send_response($data);
 
+    }
+    public function insert()
+    {
+        $name=$this->input->post('name');
+        $age=$this->input->post('age');
+        $data=$this->mongo_db->insert('student',['name'=>$name,'age'=>$age]);
+        $this->send_response($data);
+    }
+    public function delete()
+    {
+        $id=$this->input->get("id");
+        $data=$this->mongo_db->where(['_id'=>new MongoDB\BSON\ObjectID($id)])->delete('student');
+        $this->send_response($data);
+    }
+    public function update($id)
+    {
+        $name=$this->input->post('name');
+        $age=$this->input->post('age');
+        $data=$this->mongo_db->where(['_id'=>new MongoDB\BSON\ObjectID($id)])
+              ->set(['name'=>$name,'age'=>$age])
+              ->update('student');
+        $this->send_response($data);      
+
+    }
+    public function find()
+    {
+       $data=$this->mongo_db->get('student');
+       $this->send_response($data);
+        
+    }
+    public function send_response($data)
+    {
+        
         return $this->output
             ->set_content_type('application/json')
             ->set_status_header(200) // Return status
             ->set_output(json_encode($data));
-
     }
 }
+?>
